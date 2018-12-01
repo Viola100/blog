8efc24efbc0af2203ec33d49405a28a7c7de5bea
Gabriela Borowa <gabrielapiekos92@gmail.com>
2018-12-01 12:07:24 +0100

    Add abiity to create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
   def new
   end
   def create
-    render plain: params[:article].inspect
+    article_params = params.require
+    (:article).permit(:title, :text)
+    @article = Article.new (article_params)
+    @article.save
+    redirect_to articles_path
   end
 end
