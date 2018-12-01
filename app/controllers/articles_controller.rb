434559de1eca35c54d517ab10c5b210f9c3fa856
Gabriela Borowa <gabrielapiekos92@gmail.com>
2018-12-01 14:20:55 +0100

    Add validation for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,18 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
   def new
+    @article = Article.new
   end
   def create
-    article_params = params.require
-    (:article).permit(:title, :text)
+    article_params = params.require(:article).permit(:title, :text)
     @article = Article.new (article_params)
-    @article.save
-    redirect_to articles_path
+    if @article.save
+    redirect_to article_path(@article)
+    else
+    render 'new'
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
   post 'articles', to: 'articles#create'
-  get 'article/:id', to: 'articles#show', as: 'article'
+  get 'articles/:id', to: 'articles#show', as: 'article'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
