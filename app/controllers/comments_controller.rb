d4252c572a2e71a8af2eb6bef820e5c95547ed62
Gabriela Borowa <gabrielapiekos92@gmail.com>
2018-12-02 12:41:10 +0100

    create ability to delete article with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,26 @@
 class CommentsController < ApplicationController
-before_action :find_article
+ before_action :find_article
 
 
-def create
-@comment = Comment.new(comment_params)
-@comment.article = @article
-@comment.save
-redirect_to article_path(@article)
+ def create
+  @comment = Comment.new(comment_params)
+  @comment.article = @article
+  @comment.save
+  redirect_to article_path(@article)
+ end
 
-
-end
-
+ def destroy
+  @comment = Comment.find(params[:id])
+  @comment.destroy
+  redirect_to article_path(@article)
+ end
 
 private
-def comment_params
- params.require(:comment).permit(:commenter, :body)
-end
+ def comment_params
+  params.require(:comment).permit(:commenter, :body)
+ end
 
-def find_article
+ def find_article
   @article = Article.find(params[:article_id])
-end
-
+ end
 end
